f0bc77961f38ab058c30a48810c8c70cdd83506a
Makarov-AA <43129398+Makarov-AA@users.noreply.github.com>
2021-07-09 16:52:18 +0300

    Smile detector test

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from src.laugh_detector import LaughDetector
 import sys
 import cv2
 import argparse
 import logging as log
+import numpy as np
+from openvino.inference_engine import IENetwork, IECore
 
 def build_argparser():
     parser = argparse.ArgumentParser()
@@ -18,17 +19,32 @@ def build_argparser():
 def prepare_input():
     pass
 
+def recognize_smile(image, exec_net, input_blob, out_blob):
+    input = cv2.resize(image, (64, 64))
+    input = input.transpose(2, 0, 1)
+    result = image
+
+    output = exec_net.infer(inputs={input_blob : input})
+    if np.argmax(output[out_blob]) == 1:
+        cv2.putText(result, "Smile!", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
+    return image
+
 def main():
     log.basicConfig(format="[ %(levelname)s ] %(message)s",
         level=log.INFO, stream=sys.stdout)
     args = build_argparser().parse_args()
 
     cap = cv2.VideoCapture(0)
-    detector = LaughDetector()
+
+    ie = IECore()
+    net = ie.read_network(model="./models/emotions-recognition-retail-0003/FP16-INT8/emotions-recognition-retail-0003.xml")
+    exec_net = ie.load_network(network=net, device_name="CPU")
+    out_blob = next(iter(net.outputs))
+    input_blob = next(iter(net.inputs))
 
     while True:
         ret, frame = cap.read()
-        frame = detector.process(frame)
+        frame = recognize_smile(frame, exec_net, input_blob, out_blob)
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
--- src/laugh_detector.py
+++
@@ -1,6 +0,0 @@
-class LaughDetector():
-    def __init__(self):
-        pass
-
-    def process(self, image):
-        return image
